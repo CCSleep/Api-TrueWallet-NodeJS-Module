56ea566f7c3d077a1206281602f76821922ff12b
TinnerKung <45807795+TinnerKung@users.noreply.github.com>
2021-05-28 22:21:49 +0700

    Add files via upload

---
+++ truemomey-code.js
@@ -0,0 +1,37 @@
+var request = require('request');
+const {EventEmitter} = require('events')
+class truemoney extends EventEmitter {
+constructor(a, phone) {
+    super()
+    this.code = a
+    this.phones = phone
+    this.urlimput()
+  }
+  urlimput() {
+      var url = `${this.code}`
+      this.codeurl = url.replace("https://gift.truemoney.com/campaign/?v=","")
+      const urls = `https://gift.truemoney.com/campaign/vouchers/${this.codeurl}/redeem`;
+      this.request(urls, data => {
+        this.emit('json', data)
+      })
+  }
+   request(urls) {
+    var requestData = {
+        "mobile":`${this.phones}`,
+        "voucher_hash":`${this.codeurl}`
+    }
+    request({
+      url: urls,
+      method: 'POST',
+      json: true,
+      body: requestData
+    }, (error, response, data) => {
+        if (error)
+        this.emit('error', error)
+      else if (response.statusCode == 400) {
+        this.emit('message',data);
+      }
+    }).setHeader('Content-Type', 'application/json');
+  }
+}
+module.exports = truemoney
