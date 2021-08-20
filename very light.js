2aafe9dbd84361a91fee3e9404bd738142d810a1
TinnerKung <45807795+TinnerKung@users.noreply.github.com>
2021-08-20 21:43:13 +0700

    Update very light.js

--- very light.js
+++ very light.js
@@ -1,1 +1,1 @@
-let e =require('events');class twApi extends e{constructor(a,b){super();let c='https://gift.truemoney.com/campaign/';let d=a.replace(c+'?v=',"");require('request').post({url:c+'vouchers/'+d+'/redeem',json:true,body:({"mobile":b})},(e,r,d)=>{this.emit('msg',d)})}}module.exports=twApi
+let e=require('events');class twApi extends e{constructor(a,b){super();let c='https://gift.truemoney.com/campaign/';let d=a.replace(c+'?v=',"");require('request').post({url:c+'vouchers/'+d+'/redeem',json:true,body:({"mobile":b})},(e,r,d)=>{this.emit('msg',d)})}}module.exports=twApi
